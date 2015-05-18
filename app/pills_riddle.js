4afc8a57d4e6742ae419393fa0d7de80695ac144
Armen Ourfalian <aourfalian@gmail.com>
2015-05-17 18:09:59 -0700

    Added pillBoxes to rSpace and lSpace
    More consistent with the rest of the drop areas

--- app/pills_riddle.js
+++ app/pills_riddle.js
@@ -19,8 +19,7 @@ if(Math.random()>0.5){poisonWeight = -0.1};
 
 var allPills=[];//array of all the Pill objects 
 
-var playing = null;
-
+//List of Elements as Variables
 var actionWindow = document.getElementById('actionWindow');
 var pillsContainer = document.getElementById('pillsContainer');
 var group1 = document.getElementById('group1');
@@ -28,10 +27,6 @@ var group2 = document.getElementById('group2');
 var pillBox = document.getElementsByClassName('pillBox')
 var lSpace = document.getElementById('leftSpace');
 var rSpace = document.getElementById('rightSpace');
-//Create the pills in HTML
-//using the Pill class defined in Pill_class.js
-// allPills[0] is undefined, it starts from index 1
-// so that the pill ID matches its index in allPills[]
 
 var pillBoxCreator = function(container,name,count) {
 	for (var i = 0; i <count ; i++) {
@@ -39,19 +34,11 @@ var pillBoxCreator = function(container,name,count) {
 	}	
 };
 
-pillBoxCreator(pillsContainer,'A',12);
-pillBoxCreator(group1,'B',6);
-pillBoxCreator(group2,'C',6);
-
-var makePills = function() {
-	for(var i=1;i<numPills+1;i++){
-		allPills[i] = new Pill(i);
-		allPills[i].create(document.getElementById('pillBox_A'+i));
-	}
-}
-
-makePills();
-
+pillBoxCreator(pillsContainer,'Cont_',12);
+pillBoxCreator(group1,'S1_',6);
+pillBoxCreator(group2,'S2_',6);
+pillBoxCreator(lSpace,'L_',9);
+pillBoxCreator(rSpace,'R_',9);
 
 //Easy way to access each pill element by its html ID
 var pillID = function(num){
@@ -59,8 +46,20 @@ var pillID = function(num){
 }
 
 var pillBoxID = function(box,pbNum){
-	return document.getElementById('pillBox_'+box+pbNum);
+	return document.getElementById('pillBox_'+box+'_'+pbNum);
 }
+
+var makePills = function() {
+	for(var i=1;i<numPills+1;i++){
+		allPills[i] = new Pill(i);
+		// allPills[i].create(document.getElementById('pillBox_Cont_'+i));
+		allPills[i].create(pillBoxID('Cont',i));
+	}
+}
+
+makePills();
+
+
 
 
 // Move a pill from wherever it waas to a new box
@@ -92,7 +91,12 @@ var pbDragEnterListener = function(e) {
 	//Can't add 2 pills to the same pillBox
 	if(!e.currentTarget.innerHTML){
 		e.preventDefault();
+		e.currentTarget.style.backgroundColor="#112233";
 	}
+}
+
+var pbDragLeaveListener = function(e) {
+	e.currentTarget.style.backgroundColor='';
 }
 
 var pbDropListener = function(e) {
@@ -103,37 +107,16 @@ var pbDropListener = function(e) {
 	pillID(pillNumber).style.marginLeft='5px';
 	
 	movePill(pillNumber,e.currentTarget);
+	e.currentTarget.style.backgroundColor='';
 }
 
 pillBoxListeners.push(function(pbNum){
 	pillBox[pbNum].addEventListener('dragenter',pbDragEnterListener);
 	pillBox[pbNum].addEventListener('dragover',pbDragEnterListener);
+	pillBox[pbNum].addEventListener('dragleave',pbDragLeaveListener);
 	pillBox[pbNum].addEventListener('drop',pbDropListener);
 });
 
-// ==== Extra Space for Pills
-var spaceDragEnterListener = function(e) {
-	e.preventDefault();
-}
-
-
-var spaceDropListener = function(e) {
-	var data = e.dataTransfer.getData('application/pill_number');
-	data = data.split(',');
-	var parentEl = document.getElementById(data[0]);
-	var pillNumber = parseInt(data[1],10);
-	
-	movePill(pillNumber,e.currentTarget);
-	pillID(pillNumber).style.marginLeft='-15px';
-}
-
-lSpace.addEventListener('dragenter',spaceDragEnterListener);
-lSpace.addEventListener('dragover',spaceDragEnterListener);
-lSpace.addEventListener('drop',spaceDropListener);
-
-rSpace.addEventListener('dragenter',spaceDragEnterListener);
-rSpace.addEventListener('dragover',spaceDragEnterListener);
-rSpace.addEventListener('drop',spaceDropListener);
 
 // Note to Pierre,
 // The only reason I made pillBoxListeners and pillListeners as arrays is because I 
@@ -155,6 +138,29 @@ pillBoxListeners.push(function(pbNum){
 ****/
 
 
+/****** Extra Space for Pills.... I ended up putting pillBoxes here too
+var spaceDragEnterListener = function(e) {
+	e.preventDefault();
+}
+
+var spaceDropListener = function(e) {
+	var data = e.dataTransfer.getData('application/pill_number');
+	data = data.split(',');
+	var parentEl = document.getElementById(data[0]);
+	var pillNumber = parseInt(data[1],10);
+	
+	movePill(pillNumber,e.currentTarget);
+	pillID(pillNumber).style.marginLeft='-15px';
+}
+
+lSpace.addEventListener('dragenter',spaceDragEnterListener);
+lSpace.addEventListener('dragover',spaceDragEnterListener);
+lSpace.addEventListener('drop',spaceDropListener);
+
+rSpace.addEventListener('dragenter',spaceDragEnterListener);
+rSpace.addEventListener('dragover',spaceDragEnterListener);
+rSpace.addEventListener('drop',spaceDropListener);
+******/
 
 for (var i=0;i<pillBox.length;i++){
 	for (var j=0;j<pillBoxListeners.length;j++){
